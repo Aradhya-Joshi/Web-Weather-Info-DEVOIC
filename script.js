1696a06277619540dcad68b2c3e2559171944349
Aradhya <128085597+Aradhya-Joshi@users.noreply.github.com>
2023-12-06 18:54:27 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,59 @@
+let weather = {
+    apiKey: "9f779f3f19259e074b73edc6a131c540",
+    fetchWeather: async function (city) {
+      try {
+            const response = await fetch(
+                "https://api.openweathermap.org/data/2.5/weather?q=" +
+                city +
+                "&units=metric&appid=" +
+                this.apiKey
+            );
+            if (!response.ok) {
+                alert("No weather found.");
+                throw new Error("No weather found.");
+            }
+            const data = await response.json();
+            return this.displayWeather(data);
+        } catch (error) {
+            return console.error('Error fetching weather:', error);
+        }
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+          "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText =
+          "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText =
+          "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage =
+          "url('https://source.unsplash.com/1600x900/?" + name + "')";
+      },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  weather.fetchWeather("Bhopal").then(() => {
+    // You can perform additional actions here if needed
+    console.log("Weather data for Bhopal has been fetched and displayed.");
+  });
